8384d9d7fdace6940c436727a7da7f2565edc2eb
opet <opet@Academico.edu>
2022-10-21 20:58:51 -0300

    Adicionado mensagem de virificação de email e senha

--- login.php
+++ login.php
@@ -1,9 +1,14 @@
 <head>
     <link rel="stylesheet" href="styles.css">
+    <script>
+        window.onload = ()=>{
+            document.getElementById('focar').focus();
+        }
+    </script>
 </head>
 <div class="formulario">
 <form action="login.php" method="post">
-<input type="text" name="user" placeholder="Usuario">
+<input type="text" name="user" placeholder="Usuario" id="focar">
 <input type="password" name="senha" placeholder="Senha">
 <button type="submit" name="login">Logar</button>
 </form>
@@ -14,6 +19,17 @@
     if(isset($_POST['login'])){
         $user = $_POST['user'];
         $senha = $_POST['senha'];
+        $login = $conn -> prepare('SELECT * FROM `cadastro` WHERE `user_cad` = ? AND `senha_cad` = md5(?);');
+        $login -> bindParam(1, $user);
+        $login -> bindParam(2, $senha);
+        $login -> execute();
+        if($login -> rowCount()==0){
+            echo "<h2 class='titulo'>Login ou senha invalida!</h2>";
+        }else{
+            echo "<h2 class='titulo'>Deu certo</h2>";
+        }
+
     }
 
 ?>
+
