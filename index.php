ef4ba475b40b6c3b61da0b6ee0fb2f3912926b82
opet <opet@Academico.edu>
2022-10-21 21:55:09 -0300

    aula encerrada, adicionado sessao de login e encerramento de sessao

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
+<head>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
 <?php
+    session_start();
+    //verifica se a sessao esta aberta
+    if(!isset($_SESSION['login'])){
+        header('location:login.php');
+    }
     include 'conn.php';
-        
+    $id = $_SESSION['login'];
+    $consulta_nome = $conn -> prepare('SELECT * FROM cadastro WHERE id_cad = ?');
+    $consulta_nome -> bindValue(1, $id);
+    $consulta_nome -> execute();
+    $row = $consulta_nome->fetch();
+    $nome = $_SESSION['nome'];
+    echo "<h1>Bem Vindo ".$nome."</h1>";
+    echo "<a href='index.php?aviso'>Logout</a>";
+    if(isset($_GET['aviso'])){
+        echo "<br/><br/> Deseja Realmente sair?<br/>";
+        echo "<a href='index.php?logout'>Sim<a/><br/>";
+        echo "<a href='index.php'>Não<a/>";
+    }
+
+    //realizar logout
+    if(isset($_GET['logout'])){
+        session_destroy();
+        header('location:login.php');
+
+    }
 
 ?>
--- login.php
+++ login.php
@@ -1,16 +1,30 @@
 <head>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script>
         window.onload = ()=>{
             document.getElementById('focar').focus();
         }
     </script>
 </head>
+<body>
 <div class="formulario">
 <form action="login.php" method="post">
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Usuario</label>
+  <input type="text" class="form-control"  id="focar" name="user">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Senha</label>
+  <input type="password" class="form-control"  name="senha" >
+</div>
+<!--
 <input type="text" name="user" placeholder="Usuario" id="focar">
 <input type="password" name="senha" placeholder="Senha">
-<button type="submit" name="login">Logar</button>
+-->
+<button type="submit" class="btn btn-outline-success"  name="login">Logar</button>
+<!--<button type="submit" name="login">Logar</button>-->
 </form>
 </div>
 
@@ -23,13 +37,21 @@
         $login -> bindParam(1, $user);
         $login -> bindParam(2, $senha);
         $login -> execute();
+        //verifica se a senha e o usuario existem no banco de dados
         if($login -> rowCount()==0){
             echo "<h2 class='titulo'>Login ou senha invalida!</h2>";
         }else{
-            echo "<h2 class='titulo'>Deu certo</h2>";
+            //inicia sessao
+           session_start();
+           $row=$login->fetch();
+           $_SESSION['nome']= $row['nome_cad'];
+           $_SESSION['login'] = $row['id_cad'];
+           
+           //redireciona para a tela inicial
+           header('location:index.php');
         }
 
     }
 
 ?>
-
+</body>
